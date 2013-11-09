f63e3dfc5f71d3ec1818413e7b56a16ed09363c6
Shrayas <shrayasr@gmail.com>
2013-11-09 10:21:23 +0530

    Fixing multiple insert & adding data
    * Changing the insert to a multiple variety
    * Adding data to check insert items

--- app/db.py
+++ app/db.py
@@ -19,7 +19,7 @@ def add_new_item(data):
   c = db.cursor()
 
   # also supports multi insert
-  c.execute("""INSERT INTO 
+  c.executemany("""INSERT INTO 
   items(feed_id,title,description,link,guid,pubdate)
   VALUES(%d,%s,%s,%s,%s,%s)""",
   (data))
@@ -31,6 +31,27 @@ def get_feed_id(feed_url):
   return c.fetchone()[0];
 
 if __name__ == "__main__":
-   data = {'feed_url':'http://rssulr222222','title':'here\'s the title111','description':'here\'s a big badass description','link': 'http://link_url'}
-   create_new_feed(data)
+   #data = {'feed_url':'http://rssulr222222','title':'here\'s the title111','description':'here\'s a big badass description','link': 'http://link_url'}
+   data = {'link': 'http://techcrunch.com', 'description': 'Startup and Technology News', 'feed_url': 'http://techcrunch.com/feed/', 'title': 'TechCrunch'}
+   items = [
+           {
+               'description':'asdfasdf',
+               'pubdate':'2013-01-01',
+               'title':'asdasdasd',
+               'feed_id':2,
+               'link':'http://www.a.com',
+               'guid':'qq881ndnbqbq99'
+           },
+           {
+               'description':'qwerasdfasdf',
+               'pubdate':'2013-01-01',
+               'title':'asdasdasd',
+               'feed_id':2,
+               'link':'http://www.a.com',
+               'guid':'qq881ndnbqbq99xxaa'
+           }
+           ]
+  # create_new_feed(data)
+
+   add_new_item(items)
   # print get_feed_id("http://rssulr");
