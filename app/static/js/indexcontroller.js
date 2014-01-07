38a8943bb61eccda59c29347b370835b78a12f2b
Shrayas <shrayasr@gmail.com>
2014-01-07 20:39:00 +0530

    Initial working of Rsswala ui! :clap:
    * Thank you :boy: @raghothams :+1: :smile:
    * Bloody AngularJS so "D"isruptive (vishal sikka style) :older_man:

---
+++ app/static/js/indexcontroller.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var app = angular.module('rsswala',['ngRoute']);
+
+function itemsCtrl($scope, $http) {
+
+  $scope.items = [];
+
+  $scope.getFeeds = function () {
+
+      $http({
+
+        method:'GET',
+        url:'/feeds/items'
+
+      }).success(
+
+          function(data, status, headers, config) {
+            $scope.items = data;
+          }
+
+        )
+        .error(
+
+          function(data, status, headers, config) {
+            console.log("error",data);
+          }
+
+        );
+
+  };
+
+  $scope.getFeeds();
+}
