3d2da2d67c0eea57a69df71e6e5b57bc87ca943c
Shrayas <shrayasr@gmail.com>
2014-09-01 11:09:59 +0530

    Starting over

--- run.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from app import conf as config
-
-# Run the app with params picked up from the config
-app.run(
-        debug=config.DEBUG,
-        port=config.PORT,
-        host=config.HOST
-        )
